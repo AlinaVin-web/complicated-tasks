dee27bc3d8edb13d78761f646e3a3d49cf283664
Alina Vinogradova <ellafoss@yandex.ru>
2024-10-03 04:30:32 +0300

    Урок 2. Операции с числом

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+let num = 266219;
+let result = 1;
+
+num = num + "".split("");
+
+for (let v of num) {
+    result *= +v;
+}
+
+result = result ** 3 + "".split("");
+
+console.log(result[0], result[1]);
