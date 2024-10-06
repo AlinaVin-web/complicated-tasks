c6d8c4a9aeb61655c78094b4bbf9271ce3a2eba3
Alina Vinogradova <ellafoss@yandex.ru>
2024-10-06 06:08:09 +0300

    Урок 7. Вывод дней недели

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const body = document.querySelector('body');
+let now = new Date();
+let day = now.getUTCDay() - 1;
+let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+
+for (let i = 0; i < week.length; i++) {
+    let style = "";
+
+    if (i > 4) style = "font-style: italic;";
+
+    if ((day > -1 && day === i) || (day === -1 && i === 6)) style += "font-weight: 700;";
+
+    if (style) body.insertAdjacentHTML("beforeend", "<p style='" + style + "'>" + week[i] + "</p>");
+    else body.insertAdjacentHTML("beforeend", "<p>" + week[i] + "</p>");
+}
